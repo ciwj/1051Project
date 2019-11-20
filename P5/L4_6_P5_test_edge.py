1c3b0c2b4c0f1829fe2d02503ff276d6d08043f7
ciwj <55768502+ciwj@users.noreply.github.com>
2019-11-20 17:04:52 -0500

    Fixed bugs

--- P5/L4_6_P5_test_edge.py
+++ P5/L4_6_P5_test_edge.py
@@ -12,11 +12,12 @@ def test_edge():
     Cases:
     >>> edge_detection(image, 128)
     Top pixel     Bottom pixel      Result
-    (0, 0, 0)       + (255, 255, 255) -> (0, 0, 0)
-    (0, 0, 0)       + (53, 64, 100)   -> (255, 255, 255)
-    (124, 132, 122) + (0, 66, 0)      -> (0, 0, 0)
+    (0, 0, 0)       + (255, 255, 255) -> (255, 255, 255), (255, 255, 255)
+    (0, 0, 0)       + (53, 64, 100)   -> (0, 0, 0), (255, 255, 255)
+    (124, 132, 122) + (0, 66, 0)      -> (0, 0, 0), (255, 255, 255)
     """
     
+    isCorrect = True
     img = create_image(3, 2)
     
     set_color(img, 0, 0,  create_color(0, 0, 0))
@@ -28,14 +29,14 @@ def test_edge():
     
     expect = create_image(3, 2)
     
-    set_color(img, 0, 0,  create_color(0, 0, 0))
-    set_color(img, 1, 0,  create_color(255, 255, 255))
-    set_color(img, 2, 0,  create_color(0, 0, 0))
-    set_color(img, 0, 1,  create_color(255, 255, 255))
-    set_color(img, 1, 1,  create_color(0, 0, 0))
-    set_color(img, 2, 1,  create_color(0, 0, 0))
+    set_color(expect, 0, 0,  create_color(255, 255, 255))
+    set_color(expect, 1, 0,  create_color(0, 0, 0))
+    set_color(expect, 2, 0,  create_color(0, 0, 0))
+    set_color(expect, 0, 1,  create_color(255, 255, 255))
+    set_color(expect, 1, 1,  create_color(255, 255, 255))
+    set_color(expect, 2, 1,  create_color(255, 255, 255))
     
-    actual = edge_detection(img, 128)
+    actual = detect_edges(img, 128)
     
     for x, y, colour1 in expect:
         colour2 = get_color(actual, x, y)
@@ -43,4 +44,4 @@ def test_edge():
             isCorrect = False
             print('Pixel failed: ', (x, y), 'is', colour2, 'not', colour1)
     
-    return isCorrect    
+    return isCorrect
