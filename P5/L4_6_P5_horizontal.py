6921e00d3ca10e19109de9d9dc992997fb0bb34e
ciwj <55768502+ciwj@users.noreply.github.com>
2019-11-21 12:23:53 -0500

    Minor fixes

--- P5/L4_6_P5_horizontal.py
+++ P5/L4_6_P5_horizontal.py
@@ -3,9 +3,11 @@ from Cimpl import *
 # Milestone 2, P5. Group L4-6.
 # Submitted 20/11/19, created by Zeyad Bakr.
 
+
 def flip_horizontal(oldImage: Image):
     """
-    Takes a Cimpl.Image object and returns another Cimpl.Image object with the image horizontally flipped.
+    Takes a Cimpl.Image object and returns another
+    Cimpl.Image object with the image horizontally flipped.
     Created by Zeyad Bakr.
     Student # 101142932, Group L4-6.
     >>> flip1 = flip_horizontal(img)
