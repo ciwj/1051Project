b4af64443969a7e9c5ec1df1565dde3c81305d36
ciwj <55768502+ciwj@users.noreply.github.com>
2019-11-20 23:18:48 -0500

    Switched with horizontal

--- P5/L4_6_P5_vertical.py
+++ P5/L4_6_P5_vertical.py
@@ -1,19 +1,20 @@
-
 from Cimpl import *
 
 # Milestone 2, P5. Group L4-6.
 # Submitted 20/11/19, created by Callum Ullrich.
 
-def flip_vertical(oldImage: Image):
+def flip_vertically(oldImage: Image):
     """
     Takes a Cimpl.Image object and returns another Cimpl.Image object with the image vertically flipped.
     Created by Callum Ullrich.
     Student # 101148042, Group L4-6.
+    >>> flip1 = flip_vertical(img)
+    >>> flip2 = flip_vertical(img2)
     """
     image = copy(oldImage)
     
     for x, y, col in oldImage:
-        set_color(image, x, get_height(image) - (y + 1),  col)
+        set_color(image, get_width(image) - (x + 1), y,  col)
     
     show(image)
     return image
