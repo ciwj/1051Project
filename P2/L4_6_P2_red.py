10b42ca374b6cd503334dab19bb158d91c0cceb3
ciwj <55768502+ciwj@users.noreply.github.com>
2019-11-21 11:54:02 -0500

    Fixed minor issues

--- P2/L4_6_P2_red.py
+++ P2/L4_6_P2_red.py
@@ -1,39 +1,25 @@
 from Cimpl import *
 
+# Milestone 2, P5. Group L4-6.
+# Submitted 20/11/19, created by Zeyad Bakr.
+
+
 def red_channel(oldImage):
     """
-    Takes an image, and sets the green and blue channels to 0. Returns an image with only the red channel present.
+    Takes an image, and sets the green and blue channels to 0.
+    Returns an image with only the red channel present.
     Parameter is a Cimpl.Image object and a Cimpl.Image object is returned.
     Created by Zeyad Bakr.
     Student # 101142932, Group L4-6.
+    >>>red_image = red_channel(img)
+    >>>red_image2 = red_channel(img2)
     """
-     
     image = copy(oldImage)
+    
     for pixel in image:
         x, y, (r, g, b) = pixel
         newColor = create_color(r, 0, 0)
         set_color(image, x, y, newColor)
-        show(oldImage)
+    
     show(image)
     return image
-
-def test_red(oldImage):
-    """
-    Tests the red_channel function. Takes an image and returns True if the green and blue channels aren't present, returns False if there is any green or blue in the image. doctest examples are assuming oldImage has blue and green in them.
-    Created by Zeyad Bakr.
-    Student # 101142932, Group L4-6.
-    >>> test_red(red_channel(oldImage))
-    Image passed.
-    >>> test_red(oldImage)
-    Image failed.
-    """
-    image = copy(oldImage)
-    isCorrect = True
-    for pixel in image:
-            x, y, (r, g, b) = pixel
-            if not g == b == 0:
-                isCorrect = False
-    if isCorrect:
-        print("Image passed.")
-    else:
-        print("Image failed.")
