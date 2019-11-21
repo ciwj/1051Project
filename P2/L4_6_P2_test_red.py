05762e77276857c51563e5e8a802528daefe456a
ciwj <55768502+ciwj@users.noreply.github.com>
2019-11-21 11:54:57 -0500

    Fixed test function to what was expected.

--- P2/L4_6_P2_test_red.py
+++ P2/L4_6_P2_test_red.py
@@ -1,20 +1,50 @@
 from Cimpl import *
-from L4_6_P2_red import *
+from L4_6_P2_red import red_channel
 
-def test_red(oldImage):
+# Milestone 2, P5. Group L4-6.
+# Submitted 20/11/19, created by Zeyad Bakr.
+
+
+def test_red():
     """
-    Tests the red_channel function. Takes an image and returns True if the green and blue channels aren't present, returns False if there is any green or blue in the image. doctest examples are assuming oldImage has blue and green in them.
+    Tests the red_channel function.
+    Takes an image and returns True if the green and blue channels aren't
+    present, returns False if there is any green or blue in the image.
+    doctest examples are assuming oldImage has blue and green in them.
     Created by Zeyad Bakr.
     Student # 101142932, Group L4-6.
     >>> test_red(red_channel(oldImage))
     Image passed.
     >>> test_red(oldImage)
     Image failed.
+    Cases:
+    (0, 0, 0)       -> (0, 0, 0)
+    (128, 128, 128) -> (128, 0, 0)
+    (255, 255, 255) -> (255, 0, 0)
+    (0, 255, 0)     -> (0, 0, 0)
+    (255, 0, 0)     -> (255, 0, 0)
     """
-    image = copy(oldImage)
-    isCorrect = True
-    for pixel in image:
-            x, y, (r, g, b) = pixel
-            if not g == b == 0:
-                isCorrect = False
-    return isCorrect
+    is_correct = True
+    img = create_image(5, 1)
+    set_color(img, 0, 0,  create_color(0, 0, 0))
+    set_color(img, 1, 0,  create_color(128, 128, 128))
+    set_color(img, 2, 0,  create_color(255, 255, 255))
+    set_color(img, 3, 0,  create_color(0, 255, 0))
+    set_color(img, 4, 0,  create_color(255, 0, 0))
+    
+    expect = create_image(5, 1)
+    set_color(expect, 0, 0,  create_color(0, 0, 0))
+    set_color(expect, 1, 0,  create_color(128, 0, 0))
+    set_color(expect, 2, 0,  create_color(255, 0, 0))
+    set_color(expect, 3, 0,  create_color(0, 0, 0))
+    set_color(expect, 4, 0,  create_color(255, 0, 0))
+    
+    actual = red_channel(img)
+    
+    for x, y, colour1 in expect:
+        colour2 = get_color(actual, x, y)
+        if (colour1 != colour2):
+            is_correct = False
+            print('Pixel failed: ', (x, y), 'is', colour2, 'not', colour1)
+    
+    return is_correct
