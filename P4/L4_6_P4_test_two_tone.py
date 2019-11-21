c8a038b5153b4b4ddbfdf7502ba35f5913c43657
ciwj <55768502+ciwj@users.noreply.github.com>
2019-11-21 12:08:11 -0500

    Minor fixes

--- P4/L4_6_P4_test_two_tone.py
+++ P4/L4_6_P4_test_two_tone.py
@@ -1,11 +1,17 @@
 from Cimpl import *
 from L4_6_P4_image_filters import two_tone
 
+# Milestone 2, P5. Group L4-6.
+# Submitted 20/11/19, created by Zeyad Bakr.
+
+
 def test_two_tone():
     """
-    Tests the two_tone() function. Returns True if successful, otherwise returns False
+    Tests the two_tone() function.
+    Returns True if successful, otherwise returns False.
     Created by Zeyad Bakr.
     Student #: 101142932, Group L4-6
+    >>> two_toned_img = two_tone(img, "black", "white")
     Test cases:
     (0, 0, 0) -> (0, 0, 0)
     (128, 128, 128) -> (255, 255, 255)
@@ -25,7 +31,7 @@ def test_two_tone():
     set_color(expect, 0, 0,  create_color(0, 0, 0))
     set_color(expect, 1, 0,  create_color(255, 255, 255))
     set_color(expect, 2, 0,  create_color(0, 0, 0))
-    set_color(expect, 3, 0,  create_color(255, 255, 255))    
+    set_color(expect, 3, 0,  create_color(255, 255, 255))
     
     actual = two_tone(img, "black", "white")
     
