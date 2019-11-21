6ec27f137c4c65fc6ac377559496f7056a192175
ciwj <55768502+ciwj@users.noreply.github.com>
2019-11-20 23:23:34 -0500

    Switched with test vertical

--- P5/L4_6_P5_test_horizontal.py
+++ P5/L4_6_P5_test_horizontal.py
@@ -4,11 +4,15 @@ from L4_6_P5_horizontal import *
 # Milestone 2, P5. Group L4-6.
 # Submitted 20/11/19, created by Callum Ullrich.
 
-def test_horizontal():
+def test_flip_horizontal():
     """
     Tests the flip_horizontal() function. Returns True if it passes, returns False otherwise.
     Created by Callum Ullrich.
     Student # 101148042, Group L4-6
+    >>>test_flip_horizontal()
+    True
+    >>>test_flip_horizontal()
+    False
     Cases:
     (0, 0, 0)       -> (255, 0, 0)
     (128, 128, 128) -> (0, 255, 0)
@@ -17,29 +21,29 @@ def test_horizontal():
     (255, 0, 0)     -> (0, 0, 0)
     """
     
-    chillin = True
-    testimage = create_image(5, 1)
+    isCorrect = True
+    img = create_image(1, 5)
     
-    set_color(testimage, 0, 0,  create_color(0, 0, 0))
-    set_color(testimage, 1, 0,  create_color(128, 128, 128))
-    set_color(testimage, 2, 0,  create_color(255, 255, 255))
-    set_color(testimage, 3, 0,  create_color(0, 255, 0))
-    set_color(testimage, 4, 0,  create_color(255, 0, 0))    
+    set_color(img, 0, 0,  create_color(0, 0, 0))
+    set_color(img, 0, 1,  create_color(128, 128, 128))
+    set_color(img, 0, 2,  create_color(255, 255, 255))
+    set_color(img, 0, 3,  create_color(0, 255, 0))
+    set_color(img, 0, 4,  create_color(255, 0, 0))
     
-    expect = create_image(5, 1)
+    expect = create_image(1, 5)
     
-    set_color(expect, 0, 0,  create_color(255, 0, 0))
-    set_color(expect, 1, 0,  create_color(0, 255, 0))
-    set_color(expect, 2, 0,  create_color(255, 255, 255))
-    set_color(expect, 3, 0,  create_color(128, 128, 128))
-    set_color(expect, 4, 0,  create_color(0, 0, 0))
+    set_color(img, 0, 0,  create_color(255, 0, 0))
+    set_color(img, 0, 1,  create_color(0, 255, 0))
+    set_color(img, 0, 2,  create_color(255, 255, 255))
+    set_color(img, 0, 3,  create_color(128, 128, 128))
+    set_color(img, 0, 4,  create_color(0, 0, 0))
     
-    actual = flip_horizontal(testimage)
-     
+    actual = flip_horizontal(img)
+    
     for x, y, colour1 in expect:
         colour2 = get_color(actual, x, y)
         if (colour1 != colour2):
-            chillin = False
+            isCorrect = False
             print('Pixel failed: ', (x, y), 'is', colour2, 'not', colour1)
     
-    return chillin    
+    return isCorrect
