51d7cdc62c4308a701b91ca24bd20d1547adb745
ciwj <55768502+ciwj@users.noreply.github.com>
2019-11-21 12:22:01 -0500

    Minor fixes

--- P5/L4_6_P5_test_vertical.py
+++ P5/L4_6_P5_test_vertical.py
@@ -1,15 +1,17 @@
 from Cimpl import *
-from L4_6_P5_horizontal import *
+from L4_6_P5_vertical import flip_vertical
 
 # Milestone 2, P5. Group L4-6.
 # Submitted 20/11/19, created by Zeyad Bakr.
 
+
 def test_vertical():
     """
-    Tests the flip_vertical() function. Returns True if passed, False if failed.
+    Tests the flip_vertical() function.
+    Returns True if passed, False if failed.
     Created by Zeyad Bakr.
     Student # 101142932, Group L4-6
-	>>>test_flip_vertical()
+    >>>test_flip_vertical()
     True
     >>>test_flip_vertical()
     False
@@ -21,14 +23,14 @@ def test_vertical():
     (255, 0, 0)     -> (0, 0, 0)
     """
     
-    chillin = True
-    testimage = create_image(5, 1)
+    is_correct = True
+    img = create_image(5, 1)
     
-    set_color(testimage, 0, 0,  create_color(0, 0, 0))
-    set_color(testimage, 1, 0,  create_color(128, 128, 128))
-    set_color(testimage, 2, 0,  create_color(255, 255, 255))
-    set_color(testimage, 3, 0,  create_color(0, 255, 0))
-    set_color(testimage, 4, 0,  create_color(255, 0, 0))    
+    set_color(img, 0, 0,  create_color(0, 0, 0))
+    set_color(img, 1, 0,  create_color(128, 128, 128))
+    set_color(img, 2, 0,  create_color(255, 255, 255))
+    set_color(img, 3, 0,  create_color(0, 255, 0))
+    set_color(img, 4, 0,  create_color(255, 0, 0))
     
     expect = create_image(5, 1)
     
@@ -38,12 +40,12 @@ def test_vertical():
     set_color(expect, 3, 0,  create_color(128, 128, 128))
     set_color(expect, 4, 0,  create_color(0, 0, 0))
     
-    actual = flip_vertical(testimage)
-     
+    actual = flip_vertical(img)
+    
     for x, y, colour1 in expect:
         colour2 = get_color(actual, x, y)
         if (colour1 != colour2):
-            chillin = False
+            is_correct = False
             print('Pixel failed: ', (x, y), 'is', colour2, 'not', colour1)
     
-    return chillin
+    return is_correct
