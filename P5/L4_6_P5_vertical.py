e995321764197e6c565e87b739bfc914c6b8cd7d
callumullrich <58010238+callumullrich@users.noreply.github.com>
2019-11-22 16:50:58 -0500

    Fixed vertical file with checklist suggestions

--- P5/L4_6_P5_vertical.py
+++ P5/L4_6_P5_vertical.py
@@ -3,9 +3,12 @@ from Cimpl import *
 # Milestone 2, P5. Group L4-6.
 # Submitted 20/11/19, created by Callum Ullrich.
 
+
 def flip_vertically(oldImage: Image):
     """
-    Takes a Cimpl.Image object and returns another Cimpl.Image object with the image vertically flipped.
+    Takes a Cimpl.Image object and returns 
+    another Cimpl.Image object with the image 
+    vertically flipped.
     Created by Callum Ullrich.
     Student # 101148042, Group L4-6.
     >>> flip1 = flip_vertical(img)
