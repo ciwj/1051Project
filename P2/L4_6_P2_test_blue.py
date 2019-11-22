d976ea943e46792715a02e7d8fff3ff03d3ed977
callumullrich <58010238+callumullrich@users.noreply.github.com>
2019-11-22 16:58:25 -0500

    Fixed blue test function

--- P2/L4_6_P2_test_blue.py
+++ P2/L4_6_P2_test_blue.py
@@ -1,12 +1,16 @@
 from Cimpl import *
 from L4_6_P2_blue import *
 
-def test_blue(new_image):
-    """tests function to see if only blue channel is present; prints 'failed' if test failed and 'passed' if test passed. Created by Callum Ullrich.
+
+def test_blue():
     """
-    chillin = True
+    Tests function to see if only blue channel is present.
+    Prints 'failed' if test failed and 'passed' if test passed. 
+    Created by Callum Ullrich.
+    """
+    blue = True
     for pixel in new_image:
-        x, y, (r, g, b) = pixel 
+        x, y, (r, g, b) = pixel
         if not r == 0 and g == 0:
             chillin = False
-    return chillin
+    return blue
