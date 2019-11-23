4960198217a212ef613ab8af06a356a908a77cbd
Navin Kangal <58009879+Fuzzy73@users.noreply.github.com>
2019-11-23 16:41:52 -0500

    Peer edit update

--- P2/L4_6_P2_red.py
+++ P2/L4_6_P2_red.py
@@ -3,8 +3,7 @@ from Cimpl import *
 # Milestone 2, P5. Group L4-6.
 # Submitted 20/11/19, created by Zeyad Bakr.
 
-
-def red_channel(oldImage: Image):
+def red_channel(oldImage: Image) -> Image:
     """
     Takes an image, and sets the green and blue channels to 0.
     Returns an image with only the red channel present.
@@ -14,12 +13,13 @@ def red_channel(oldImage: Image):
     >>>red_image = red_channel(img)
     >>>red_image2 = red_channel(img2)
     """
-    image = copy(oldImage)
+    image = copy(oldImage) #Makes a copy of the uploded image and calls it image
     
-    for pixel in image:
+    for pixel in image: #For loop changes the coulour of each pixel to only its red component
         x, y, (r, g, b) = pixel
         newColor = create_color(r, 0, 0)
         set_color(image, x, y, newColor)
     
-    show(image)
+    show(oldImage) #Show's the original image
+    show(image) #Show's the filtered image
     return image
