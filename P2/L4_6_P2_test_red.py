06a222b6c1cf820a468636159d2435d6e6999c2a
ciwj <55768502+ciwj@users.noreply.github.com>
2019-11-24 00:35:49 -0500

    Reviewed

--- P2/L4_6_P2_test_red.py
+++ P2/L4_6_P2_test_red.py
@@ -4,7 +4,8 @@ from L4_6_P2_red import red_channel
 # Milestone 2, P5. Group L4-6.
 # Submitted 20/11/19, created by Zeyad Bakr.
 
-def test_red():
+
+def test_red() -> bool:
     """
     Tests the red_channel function.
     Takes an image and returns True if the green and blue channels aren't
