6b8287fdb431aa9a5d8b98e2e5ab271cc7908038
Navin Kangal <58009879+Fuzzy73@users.noreply.github.com>
2019-11-24 18:41:44 -0500

    Update L4_6_P2_test_green.py

--- P2/L4_6_P2_test_green.py
+++ P2/L4_6_P2_test_green.py
@@ -1,13 +1,17 @@
 from Cimpl import *
 from L4_6_P2_green import *
 
+
 def test_green(new_image):
     """ Name: Navin Kangal
     Student#: 101140794
     Team: L4-6
-    Functions test whether or not there is blue or red still in any pixel in the picture.
-    If there is blue or red in any pixel the program will return "Image failed test" if
-    the image only has green in all of its pixels it will return "Image passed test".
+    Functions test whether or not there is blue
+    or red still in any pixel in the picture.
+    If there is blue or red in any pixel the 
+    program will return "Image failed test" if
+    the image only has green in all of its pixels
+    it will return "Image passed test".
     """
     for pixel in (new_image):
         x, y, (r, g, b) = pixel
