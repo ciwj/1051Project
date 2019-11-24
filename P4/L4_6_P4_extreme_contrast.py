a671933dd00c2c850c008ad200713ef1b40e212f
Navin Kangal <58009879+Fuzzy73@users.noreply.github.com>
2019-11-24 18:47:29 -0500

    Update L4_6_P4_extreme_contrast.py

--- P4/L4_6_P4_extreme_contrast.py
+++ P4/L4_6_P4_extreme_contrast.py
@@ -2,8 +2,9 @@ from Cimpl import *
 
 image = load_image(choose_file())
 
-def extreme_contrast(image):
-    """ 
+
+def extreme_contrast(image: Image) ->Image:
+    """
     Navin Kangal
     101140798
     The function takes an image and makes each colour in each pixel either 0 or 255 to create an extreme contrast.
@@ -13,19 +14,19 @@ def extreme_contrast(image):
         red = r
         green = g
         blue = b
-        if 0<=r<=127:
-            red = 0           
-        if 128<=r<=255:
-            red = 255          
-        if 0<=g<=127:
-            green = 0           
-        if 128<=g<=255:
+        if 0 <= r <= 127:
+            red = 0
+        if 128 <= r <= 255:
+            red = 255
+        if 0 <= g <= 127:
+            green = 0
+        if 128 <= g <= 255:
             green = 255
-        if 0<=b<=127:
-            blue = 0            
-        if 128<=b<=255:
+        if 0 <= b <= 127:
+            blue = 0
+        if 128 <= b <= 255:
             blue = 255
         new_colour = create_color(red, green, blue)
-        set_color (new_image, x, y, new_colour)        
+        set_color(new_image, x, y, new_colour)
     show(new_image)
     return test_extreme_contrast(new_image)
