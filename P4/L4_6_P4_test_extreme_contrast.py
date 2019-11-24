e7f2e60d108876d3ac8f7d56177b876cac907fb7
Navin Kangal <58009879+Fuzzy73@users.noreply.github.com>
2019-11-24 18:50:43 -0500

    Update L4_6_P4_test_extreme_contrast.py

--- P4/L4_6_P4_test_extreme_contrast.py
+++ P4/L4_6_P4_test_extreme_contrast.py
@@ -2,20 +2,24 @@ from Cimpl import *
 
 image = load_image(choose_file())
 
-def test_extreme_contrast(new_image):
+
+def test_extreme_contrast(new_image: Image) ->Image:
     """
     Navin Kangal
     101140798
-    The function test whether or not each colour is at 0 or 255. If a pixel has a number other than 0 or 255 the function will return "Image failed test"
+    The function test whether or not each colour
+    is at 0 or 255. If a pixel has a number other
+    than 0 or 255 the function will return "Image
+    failed test"
     """
     test = True
     for x, y, ( r, g, b) in new_image:
-        red = (r==0 or r==255)
-        green = (g==0 or g==255)
-        blue = (b==0 or b==255)
+        red = (r == 0 or r == 255)
+        green = (g == 0 or g == 255)
+        blue = ( b== 0 or b == 255)
         if not (red and green and blue):
-            test=False       
-    if test==True:
+            test = False
+    if test == True:
         print("Image passsed test")
-    if test==False:
+    if test == False:
         print("Image failed test")
